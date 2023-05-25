025336a3b7c9ce37a72de8832ba83db6c3654415
Vlad Filimon <vlad.filimon-ext@power.cloud>
2023-05-25 17:14:34 +0300

    Initial commit

---
+++ AclEntitiesBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace VladFilimon\AclEntitiesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class AclEntitiesBundle extends AbstractBundle
+{
+}
